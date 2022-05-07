8d08df86d5a33727e52fcf76e91b5df71580d78e
Brian Parker <brian.parker@hotmail.com.au>
2022-05-08 03:56:10 +1000

    Code Rub

--- BlazorBootstrapIcons/Views/Components/BlazorBootstrapIconGenerator.cs
+++ BlazorBootstrapIcons/Views/Components/BlazorBootstrapIconGenerator.cs
@@ -8,15 +8,24 @@ public abstract class BaseIcon : ComponentBase
     [Parameter]
     public int Size { get; set; } = 24;
 
+    [Parameter(CaptureUnmatchedValues = true)]
+    public Dictionary<string, object> UncapturedAttributes { get; set; }
+
     protected abstract string IconSource { get; }
 
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
-        builder.OpenElement(sequence: 1, elementName: "img");
-        builder.AddAttribute(sequence: 2, name: "src", value: $"_content/BlazorBootstrapIcons/images/bootstrap-icons/{IconSource}.svg");
+        var source = $"_content/BlazorBootstrapIcons/images/bootstrap-icons/bootstrap-icons.svg#{IconSource}";
+
+        builder.OpenElement(sequence: 1, elementName: "svg");
+        builder.AddAttribute(sequence: 2, name: "class", value: "bi");
         builder.AddAttribute(sequence: 3, name: "width", value: Size);
         builder.AddAttribute(sequence: 4, name: "height", value: Size);
-        builder.AddAttribute(sequence: 5, name: "loading", value: "lazy");
+        builder.AddAttribute(sequence: 5, name: "fill", value: "currentColor");
+
+        builder.OpenElement(sequence: 6, elementName: "use");
+        builder.AddAttribute(sequence: 7, name: "href", value: source);
+        builder.CloseElement();
         builder.CloseElement();
     }
 }
